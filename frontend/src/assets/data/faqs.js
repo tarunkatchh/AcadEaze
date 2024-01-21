1fcb4d81453a53182e773ec144c256b937c63248
Tarun Kumar Katchh <tarunkatchh.mb@gmail.com>
2024-01-22 02:11:03 +0530

    added projects from local sys to github

---
+++ frontend/src/assets/data/faqs.js
@@ -0,0 +1,25 @@
+export const faqs = [
+  {
+    question: "What is your Mentorship Program?",
+    content:
+      "One Mentor was founded on a better model of care one designed around mentees needs that provides a higher level of quality and service affordably. We do this through innovative design, excellent customer service, and the efficient use of technology.",
+  },
+  
+  {
+    question: "What are the transaction charges?",
+    content:
+      "Payment gateways like Stripe and PayPal typically charge around 3% of the transaction amount. This is beyond our control.",
+  },
+  {
+    question: "How much can I charge for my sessions?",
+    content:
+      "It really depends on how much value you think you're providing and how much are your followers willing to pay. For idea, our creators charge anywhere from $50 to $500 for a session",
+  },
+  
+
+  {
+    question: "Wait, is AcadEaze really free?",
+    content:
+      "Yes! We're free to use and charge a very tiny commission on your earnings (5%). So that we grow only when you grow",
+  },
+];
